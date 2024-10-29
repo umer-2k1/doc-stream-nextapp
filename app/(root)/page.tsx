67c6e2a52f731b766527edb499a574dd30c287fa
umer <umermemon4648@gmail.com>
2024-10-29 23:57:14 +0500

    chore: add main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <Notifications />
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
 
-export default Home
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn userId={"none"} email={"jhone@gmail.com"} />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className="document-list-item">
+                <Link
+                  href={`/documents/${id}`}
+                  className="flex flex-1 items-center gap-4"
+                >
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                    <Image
+                      src="/assets/icons/doc.svg"
+                      alt="file"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">
+                      Created about {dateConverter(createdAt)}
+                    </p>
+                  </div>
+                </Link>
+                <DeleteModal roomId={id} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn userId={"none"} email={"jhone@gmail.com"} />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Home;
