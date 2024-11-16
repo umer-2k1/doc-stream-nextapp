709924fe9b26b05de7431aef8953b5b6329b88d3
umer-2k1 <umermemon4648@gmail.com>
2024-11-16 14:01:30 +0500

    chore: add documents page

--- app/(root)/documents/page.tsx
+++ app/(root)/documents/page.tsx
@@ -1,56 +1,88 @@
-import AddDocumentBtn from "@/components/common/AddDocumentBtn";
-import { Box } from "@/components/ui/box";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import { DeleteModal } from "@/components/DeleteModal";
+import Header from "@/components/Header";
+import Notifications from "@/components/Notifications";
+import { getDocuments } from "@/lib/actions/room.actions";
+import { dateConverter } from "@/lib/utils";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { redirect } from "next/navigation";
 
-const Documents = () => {
-  const documents = [1, 2, 3];
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const roomDocuments = await getDocuments(
+    clerkUser.emailAddresses[0].emailAddress,
+  );
+
   return (
-    <Box className="px-24 py-24">
-      {documents.length > 0 ? (
-        <Box className="document-list-container">
-          <Box className="document-list-title">
-            <h3 className="text-28-semibold">All Documents</h3>
-            <AddDocumentBtn documents={documents}/>
-          </Box>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <Notifications />
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
           <ul className="document-ul">
-            {documents?.map((i, index) => (
-              <li key={index} className="document-list-item">
-                <Link href={"#"} className="flex flex-1 items-center gap-4">
-                  <Box className="hidden rounded-md bg-dark-500 p-2 sm:block">
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className="document-list-item">
+                <Link
+                  href={`/documents/${id}`}
+                  className="flex flex-1 items-center gap-4"
+                >
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
                     <Image
-                      src={"/assets/icons/doc.svg"}
-                      alt="File"
+                      src="/assets/icons/doc.svg"
+                      alt="file"
                       width={40}
                       height={40}
                     />
-                  </Box>
-                  <Box className="space-y-1">
-                    <p className="line-clamp-1 text-lg">New Title</p>
+                  </div>
+                  <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
                     <p className="text-sm font-light text-blue-100">
-                      Created about 7 hours ago
+                      Created about {dateConverter(createdAt)}
                     </p>
-                  </Box>
+                  </div>
                 </Link>
+                <DeleteModal roomId={id} />
               </li>
             ))}
           </ul>
-        </Box>
+        </div>
       ) : (
-        <Box className="document-list-empty">
+        <div className="document-list-empty">
           <Image
-            src={"/assets/icons/doc.svg"}
+            src="/assets/icons/doc.svg"
             alt="Document"
             width={40}
             height={40}
             className="mx-auto"
           />
-          <AddDocumentBtn documents={documents} />
-        </Box>
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
       )}
-    </Box>
+    </main>
   );
 };
 
-export default Documents;
+export default Home;
